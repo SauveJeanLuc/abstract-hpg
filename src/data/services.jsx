9ed410ba1f408461b358bd71b94076c8b1b78703
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-18 17:42:18 +0200

    services dummy data

---
+++ src/data/services.jsx
@@ -0,0 +1,34 @@
+let services = [
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Fighting',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Bowing',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Fighting',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Bowing',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Fighting',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+    {
+        imageUrl: 'https://static.vecteezy.com/system/resources/previews/002/648/104/original/flame-logo-template-fire-logo-design-graphic-free-vector.jpg',
+        title: 'Fire-Bowing',
+        description: 'We reach you even before the fire burns. Immediate  Connection to fire fighters who saves Life.',
+    },
+]
+
+export default services;
