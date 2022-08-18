b1c71a2a5a65d3543a945925af3a5a986b2924be
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-18 18:25:19 +0200

    footer links

---
+++ src/data/links.jsx
@@ -0,0 +1,72 @@
+let links = [
+    {   
+        title: 'Abstract',
+        links: [
+            {
+                url: '#',
+                title: 'Start Trial'
+            },
+            {
+                url: '#',
+                title: 'Pricing'
+            },
+            {
+                url: '#',
+                title: 'Download'
+            }
+        ],
+    },
+    {   
+        title: 'Resources',
+        links: [
+            {
+                url: '#',
+                title: 'Start Trial'
+            },
+            {
+                url: '#',
+                title: 'Pricing'
+            },
+            {
+                url: '#',
+                title: 'Download'
+            }
+        ],
+    },
+    {   
+        title: 'Community',
+        links: [
+            {
+                url: '#',
+                title: 'Facebook'
+            },
+            {
+                url: '#',
+                title: 'LinkedIn'
+            },
+            {
+                url: '#',
+                title: 'Instagram'
+            }
+        ],
+    },
+    {   
+        title: 'Company',
+        links: [
+            {
+                url: '#',
+                title: 'Facebook'
+            },
+            {
+                url: '#',
+                title: 'LinkedIn'
+            },
+            {
+                url: '#',
+                title: 'Instagram'
+            }
+        ],
+    },
+]
+
+export default links;
